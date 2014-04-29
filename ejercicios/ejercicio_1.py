90da6187a0a6d1440dd24b406d0b2491ea9e40c7
aolmedo <aolmedo@bellavista.(none)>
2014-04-28 23:06:30 -0300

    Segunda clase primera versión. |

--- ejercicios/ejercicio_1.py
+++ ejercicios/ejercicio_1.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python
 # -*- coding: utf-8 
 
-#def ejercicio_1():
-a = raw_input("Ingrese un numero: ")
-b = raw_input("Ingrese otro numero: ")
-c = raw_input("Ingrese otro numero: ")
-a = int(a)
-b = int(b)
-c = int(c)
+def ejercicio_1():
+    a = raw_input("Ingrese un numero: ")
+    b = raw_input("Ingrese otro numero: ")
+    c = raw_input("Ingrese otro numero: ")
+    a = int(a)
+    b = int(b)
+    c = int(c)
 
-ret = a
-if a > b:
-    if a > c:
-        ret = a
+    ret = a
+    if a > b:
+        if a > c:
+            ret = a
+        else:
+            ret = c
     else:
-        ret = c
-else:
-    if b > c:
-        ret = b
-    else:
-        ret = c
-print "El mayor de los numeros es: %s" % ret
+        if b > c:
+            ret = b
+        else:
+            ret = c
+    return "El mayor de los numeros es: %s" % ret
+
+print ejercicio_1()

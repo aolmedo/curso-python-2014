b8cc299e0279584b772e334cb6af3f4efcb276ad
aolmedo <aolmedo@bellavista.(none)>
2014-04-29 21:03:04 -0300

    Primera versión del modelo de la agenda

---
+++ agenda/model/agenda.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Agenda(object):
+    """Modelo de la agenda
+    """
+    def __init__(self):
+        self.contacto = []
+    
+    def agregar(self, unContacto):
+        pass
+
+    def modificar(self, unContacto):
+        pass
+
+    def eliminar(self, unContacto):
+        pass
+
+    def importar(self, source):
+        pass
+
+    def exportar(self, target):
+        pass
+
---
+++ agenda/model/contacto.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Contacto(object):
+    """Modelo de un Contacto de la agenda
+    """
+    def __init__(self):
+	self.nombre = ""
+	self.apellido = ""
+	self.fecha_nacimiento = ""
+	self.telefono = ""
+	self.email = ""
+	self.tweeter = ""
+	self.skype = ""
+
+    def descripcion(self);
+    	pass
